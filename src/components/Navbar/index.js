79af088d9efb3cd1dc7302474b4eba4da8f2adc6
Zimam Sayeed <zmmsayeed@gmail.com>
2022-03-19 16:00:31 +0530

    Font changed, scroll effect navbar added
    Deleted huge file

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,33 +1,50 @@
+import { useState } from 'react';
 import { websiteLogoText } from '../../constants';
 import './style.css';
 
 function Navbar() {
+  const [colorChange, setColorChange] = useState(false);
+
+  const changeNavbarColor = () =>{
+    if(window.scrollY >= 80){
+      setColorChange(true);
+    }
+    else{
+      setColorChange(false);
+    }
+  };
+
+  window.addEventListener('scroll', changeNavbarColor);
+
   return (
     <>
-      <nav className="container navbar navbar-expand-lg p-md-4 p-lg-4 p-0 navbar-light bg-transparent fixed-top">
-        <a className="navbar-brand" href="#">{websiteLogoText}</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav className={ colorChange ? "navbar navbar-expand-lg p-md-4 p-lg-4 p-0 navbar-dark bg-dark fixed-top" : "navbar navbar-expand-lg p-md-4 p-lg-4 p-0 navbar-dark bg-transparent fixed-top"  }>
+         {/* className="navbar navbar-expand-lg p-md-4 p-lg-4 p-0 navbar-dark bg-dark fixed-top"> */}
+        <div className='container'>
+          <a className="navbar-brand" href="#">{websiteLogoText}</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Music</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#about">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">EPK</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Music</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">EPK</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
--- src/sections/MainSection/index.js
+++ src/sections/MainSection/index.js
@@ -1,52 +1,63 @@
+import { useRef, useEffect } from 'react';
 import { FaInstagram, FaYoutube, FaTwitter, FaSoundcloud } from 'react-icons/fa';
 import './style.css';
 
 function MainSection() {
+    const vidRef=useRef();
+
+    useEffect(() => { vidRef.current.play(); },[]);
+
     return (
         <div id="main-section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-6 col-lg-6  main-section-content">
-                        <h1 className='mb-4 font-weight-bold'>Singer - Songwriter</h1>
-                        <h4 className='tagline'>
-                            "I do music for the love of it, and that's about it."
-                        </h4>
+            <video autoplay muted loop id="myVideo" ref={ vidRef }>
+                <source src="/videos/FOA1.mp4" type="video/mp4" />
+            </video>
+
+            <div className='content'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-6 col-lg-6  main-section-content">
+                            <h1 className='mb-4 font-weight-bold'>Singer - Songwriter</h1>
+                            <h4 className='tagline'>
+                                "I do music for the love of it, and that's about it."
+                            </h4>
 
 
-                        <div className="social-media-wrapper mt-5">
-                            <a href="https://www.instagram.com/frizzell.dsouza" target="_blank" rel="noopener noreferrer" className='social-box'>
-                                <FaInstagram size={25} />
-                            </a>
-                            <a href="https://www.youtube.com/channel/UCpDjYVQS411CBBw0thoB0Sw" target="_blank" rel="noopener noreferrer" className='social-box'>
-                                <FaYoutube size={28} />
-                            </a>
-                            <a href="https://twitter.com/toothpickyy" target="_blank" rel="noopener noreferrer" className='social-box'>
-                                <FaTwitter size={25} />
-                            </a>
-                            <a href="https://soundcloud.com/frizzelldsouza" target="_blank" rel="noopener noreferrer" className='social-box'>
-                                <FaSoundcloud size={35} />
-                            </a>
-                        </div>
+                            <div className="social-media-wrapper mt-5">
+                                <a href="https://www.instagram.com/frizzell.dsouza" target="_blank" rel="noopener noreferrer" className='social-box'>
+                                    <FaInstagram size={25} />
+                                </a>
+                                <a href="https://www.youtube.com/channel/UCpDjYVQS411CBBw0thoB0Sw" target="_blank" rel="noopener noreferrer" className='social-box'>
+                                    <FaYoutube size={28} />
+                                </a>
+                                <a href="https://twitter.com/toothpickyy" target="_blank" rel="noopener noreferrer" className='social-box'>
+                                    <FaTwitter size={25} />
+                                </a>
+                                <a href="https://soundcloud.com/frizzelldsouza" target="_blank" rel="noopener noreferrer" className='social-box'>
+                                    <FaSoundcloud size={35} />
+                                </a>
+                            </div>
 
-                        <div className='subscribe-form mt-5'>
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <form className="form-inline" action="/search" method="get">
-                                        <div className="input-group flex-fill">
-                                            <input type="email" name="subscribe" value=""
-                                                placeholder="Subsribe to get the latest updates" className="form-control subscribe-input" />
-                                            <div className="input-group-append">
-                                                <input type="submit" name="commit" value="Subscribe" className="btn btn-primary subscribe-button" data-disable-with="Subscribe" />
+                            <div className='subscribe-form mt-5'>
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <form className="form-inline" action="/search" method="get">
+                                            <div className="input-group flex-fill">
+                                                <input type="email" name="subscribe" value=""
+                                                    placeholder="Subsribe to get the latest updates" className="form-control subscribe-input" />
+                                                <div className="input-group-append">
+                                                    <input type="submit" name="commit" value="Subscribe" className="btn btn-primary subscribe-button" data-disable-with="Subscribe" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="col-12 col-md-6 col-lg-6 img-div">
-                        <img src="/images/bg2.png" alt="" className='img-fluid' />
+                        {/* <div className="col-12 col-md-6 col-lg-6 img-div">
+                            <img src="/images/bg2.png" alt="" className='img-fluid' />
+                        </div> */}
                     </div>
                 </div>
             </div>
