026f3473551107d5f5cc8133d0648984e997cf61
Zimam Sayeed <zmmsayeed@gmail.com>
2022-04-22 16:15:00 +0530

    Completed music page

--- src/sections/Music/index.js
+++ src/sections/Music/index.js
@@ -1,123 +1,160 @@
-import { useState } from 'react';
-import { Drawer } from 'antd';
+import { useState } from "react";
+import { Drawer } from "antd";
 
 // importing icons
-import { FaPlayCircle, FaApple, FaYoutube, FaSpotify } from 'react-icons/fa';
+import { FaPlayCircle, FaApple, FaYoutube, FaSpotify } from "react-icons/fa";
 
 // importing stylesheet
-import './style.css';
+import "./style.css";
 
 function Music() {
+  const [visible, setVisible] = useState(false);
+  const [song, setSong] = useState("");
+  const [selectedSong, setSelectedSong] = useState({});
 
-    const [visible, setVisible] = useState(false);
-    const [song, setSong] = useState('');
-    const [selectedSong, setSelectedSong] = useState({});
-    
-    const showDrawer = (music) => {
-        setVisible(true);
-        let str = music.name + " (" + music.sub + ")";
-        setSong(str);
-        setSelectedSong(music);
-    };
+  const showDrawer = (music) => {
+    setVisible(true);
+    let str = music.name + " (" + music.sub + ")";
+    setSong(str);
+    setSelectedSong(music);
+  };
 
-    const onClose = () => {
-        setVisible(false);
-    };
+  const onClose = () => {
+    setVisible(false);
+  };
 
-    const musicArr = [
-        {
-            name: "New",
-            sub: "Single",
-            image: "/images/music/New_vinyl.png",
-            yt: "google.com",
-            spotify: "google.com",
-            apple: "google.com",
-        },
-        {
-            name: "Drown Away",
-            sub: "Single",
-            image: "/images/music/Drown_vinyl.png",
-            yt: "google.com",
-            spotify: "google.com",
-            apple: "google.com",
-        },
-        {
-            name: "Foolish Once Again",
-            sub: "Single",
-            image: "/images/music/foolish_vinyl.png",
-            yt: "google.com",
-            spotify: "google.com",
-            apple: "google.com",
-        },
-        {
-            name: "Fall",
-            sub: "Single",
-            image: "/images/music/Fall_vinyl.png",
-            yt: "google.com",
-            spotify: "google.com",
-            apple: "google.com",
-        },
-    ]
+  const musicArr = [
+    {
+      name: "New",
+      sub: "Single",
+      image: "/images/music/New_vinyl.png",
+      yt: "google.com",
+      spotify: "google.com",
+      apple: "google.com",
+      exclusive: false,
+    },
+    {
+      name: "Drown Away",
+      sub: "Single",
+      image: "/images/music/Drown_vinyl.png",
+      yt: "google.com",
+      spotify: "google.com",
+      apple: "google.com",
+      exclusive: false,
+    },
+    {
+      name: "Foolish Once Again",
+      sub: "Single",
+      image: "/images/music/foolish_vinyl.png",
+      yt: "google.com",
+      spotify: "google.com",
+      apple: "google.com",
+      exclusive: false,
+    },
+    {
+      name: "Fall",
+      sub: "Single",
+      image: "/images/music/Fall_vinyl.png",
+      yt: "google.com",
+      spotify: "google.com",
+      apple: "google.com",
+      exclusive: true,
+    },
+  ];
 
-    return (
-        <div id="music">
-
-            <Drawer title={song} placement="bottom" onClose={onClose} visible={visible} closable>
-                <div className='drawer-div'>
-                    <a href={selectedSong.apple} className='music-link'>
-                        <FaApple size={100} style={{ color: '#000000' }}  /> 
-                    </a>
-                    <a href={selectedSong.yt} className='music-link'>
-                        <FaYoutube size={100} style={{ color: '#FF0000' }} />
-                    </a>
-                    <a href={selectedSong.spotify} className='music-link'>
-                        <FaSpotify size={100} style={{ color: '#1DB954' }} />
-                    </a>
-                </div>
-            </Drawer>
-
-            <div className='container py-5'>
-                <h1 className='mb-5'>Music</h1>
-
-                {/* <div className='row'>
-                    {
-                        musicArr.map((music, index) => (
-                            <div className='col-md-3' key={index}>
-                                <div class="card" onClick={() => showDrawer(music)}>
-                                    <img class="card-img-top" src={music.image} alt="Card image cap" />
-                                    <div className='music-info'>
-                                        <div className='row'>
-                                            <div className='col-md-3'>
-                                                <FaPlayCircle size={65} className="playButton" />    
-                                            </div>    
-                                            <div className='col-md-9'>
-                                                <p className='m-0 music-title'>{music.name}</p>
-                                                <p className='song-info'>Single</p>    
-                                            </div>   
-                                        </div> 
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div> */}
-
-                <div className='row'>
-                    {
-                        musicArr.map((music, index) => (
-                            <div className='col-md-3' key={index}>
-                                <div class="card" onClick={() => showDrawer(music)}>
-                                    <img class="card-img-top" src={music.image} alt="Card cap" />
-                                    <p className='mb-0 mt-2 music-title'>{music.name} |</p>
-                                    <p className='music-title'>{music.sub}</p>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
+  return (
+    <div id="music">
+      <Drawer
+        title={song}
+        placement="bottom"
+        onClose={onClose}
+        visible={visible}
+        closable
+      >
+        <div className="drawer-div">
+          <a href={selectedSong.apple} className="music-link">
+            <FaApple size={100} style={{ color: "#000000" }} />
+          </a>
+          <a href={selectedSong.yt} className="music-link">
+            <FaYoutube size={100} style={{ color: "#FF0000" }} />
+          </a>
+          <a href={selectedSong.spotify} className="music-link">
+            <FaSpotify size={100} style={{ color: "#1DB954" }} />
+          </a>
         </div>
-    );
+      </Drawer>
+
+      <div className="container py-5">
+        <h1 className="mb-5">Music</h1>
+
+        <div className="row">
+          {musicArr.map((music, index) => (
+            <div className="col-md-3" key={index}>
+              <div class="card" onClick={() => showDrawer(music)}>
+                <div class="dropdown">
+                  <div class="dropbtn">
+                    <img
+                      class="card-img-top"
+                      src={music.image}
+                      alt="Card cap"
+                    />
+                    <p className="mb-0 mt-2 music-title">{music.name} |</p>
+                    <p className="mb-0 music-title">{music.sub}</p>
+                  </div>
+                  <div class="dropdown-content">
+                    {music.exclusive ? (
+                      <>
+                        <p>Available exclusively on</p>
+                        <a href="#" className="mb-2">
+                          <img
+                            src="/images/audious.png"
+                            alt="spotify"
+                            width="40px"
+                          />
+                        </a>
+                      </>
+                    ) : (
+                      <>
+                        <p className="mb-2">Listen on</p>
+                        <a href="#" className="mb-2">
+                          <img
+                            src="/images/spotify.png"
+                            alt="spotify"
+                            width="20px"
+                          />
+                        </a>
+                        <a href="##" className="mb-2">
+                          <img
+                            src="/images/apple.png"
+                            alt="spotify"
+                            width="20px"
+                          />
+                        </a>
+                        <a href="###" className="mb-2">
+                          <img
+                            src="/images/amazon.png"
+                            alt="spotify"
+                            width="40px"
+                          />
+                        </a>
+                        <a href="####" className="mb-2">
+                          <img
+                            src="/images/youtube.png"
+                            alt="spotify"
+                            width="25px"
+                          />
+                        </a>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Music;
--- src/sections/Subscribe/index.js
+++ src/sections/Subscribe/index.js
@@ -1,6 +1,17 @@
+import moment from "moment";
+import { Country, State, City } from "country-state-city";
 import "./style.css";
 
 function Subscribe() {
+  let months = moment.months();
+  let countries = Country.getAllCountries()
+  let states = State.getAllStates()
+  let cities = City.getAllCities()
+  let countryStateList = getCountryStateList(
+    countries,
+    states,
+    cities
+  );
   return (
     <div id="subscribe" className="subscribe">
       <div className="row">
@@ -18,9 +29,81 @@ function Subscribe() {
         </div>
 
         <div className="col-4"></div>
+
+        <div className="col-md-4 col-lg-4 col-12 offset-md-4 offset-lg-4 mt-2">
+          <input type="text" placeholder="Enter Your Email Address" />
+        </div>
+
+        <div className="col-4"></div>
+
+        <div className="col-md-2 col-lg-2 col-12 offset-md-4 offset-lg-4 mt-4">
+          <h5>Country</h5>
+        </div>
+        <div className="col-md-2 col-lg-2 col-12 mt-4">
+          <h5>
+            State <span className="montserrat">/</span> City
+          </h5>
+        </div>
+
+        <div className="col-4"></div>
+
+        <div className="col-md-2 col-lg-2 col-12 offset-md-4 offset-lg-4 mt-2">
+          <select name="country" id="country">
+            <option value="" selected disabled>
+              Select Country
+            </option>
+            {
+              countries.map((country, index) => (
+                <option key={index} value={country.name}>{country.name}</option>
+              ))  
+            }
+          </select>
+        </div>
+        <div className="col-md-2 col-lg-2 col-12 mt-2">
+          <select name="country" id="country">
+            <option value="" selected disabled>
+              Select State/City
+            </option>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+          {/* <input type="text" placeholder="Select a country/region" /> */}
+        </div>
+
+        <div className="col-4"></div>
+
+        <div className="col-md-4 col-lg-4 col-12 offset-md-4 offset-lg-4 mt-4">
+          <h5>Birthday</h5>
+        </div>
+        <div className="col-4"></div>
+
+        <div className="col-md-2 col-lg-2 col-12 offset-md-4 offset-lg-4 mt-2">
+          <select name="month" id="month">
+            <option value="" selected disabled>
+              Month
+            </option>
+            {months.map((month, index) => (
+              <option key={index} value={index}>
+                {month}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col-md-1 col-lg-1 col-12 mt-2">
+          <input type="number" placeholder="Year" />
+        </div>
+        <div className="col-md-1 col-lg-1 col-12 mt-2">
+          <input type="number" placeholder="Day" />
+        </div>
+
+        <div className="col-4"></div>
       </div>
     </div>
   );
 }
+
+function getCountryStateList(countries, states, city) {}
 
 export default Subscribe;
