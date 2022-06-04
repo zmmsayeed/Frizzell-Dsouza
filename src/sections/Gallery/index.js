730081035225dce093dafe410feb141d72fdac22
Zimam Sayeed <zmmsayeed@gmail.com>
2022-06-04 15:58:48 +0530

    Finished the gallery page

--- src/sections/Gallery/index.js
+++ src/sections/Gallery/index.js
@@ -3,7 +3,30 @@ import './style.css';
 function Gallery({refSection}) {
     return (
         <div id='gallery' ref={refSection}>
-            <h1>Gallery Page</h1>
+            <div className='container'>
+                <h2 className="mb-5">Gallery</h2>
+
+                <div className='row'>
+                    <div className='col-md-4 mb-3 img-holder'>
+                        <img src='/images/Gallery/1.jpg' className='img-fluid' />
+                    </div>
+                    <div className='col-md-4 mb-3 img-holder'>
+                        <img src='/images/Gallery/2.jpg' className='img-fluid' />
+                    </div>
+                    <div className='col-md-4 mb-3 img-holder'>
+                        <img src='/images/Gallery/3.jpg' className='img-fluid' />
+                    </div>
+                    <div className='col-md-4 mb-3 img-holder'>
+                        <img src='/images/Gallery/4.jpg' className='img-fluid' />
+                    </div>
+                    <div className='col-md-5 mb-3 img-holder'>
+                        <img src='/images/Gallery/6.jpg' className='img-fluid' />
+                    </div>
+                    <div className='col-md-3 mb-3 img-holder'>
+                        <img src='/images/Gallery/5.jpg' className='img-fluid' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
