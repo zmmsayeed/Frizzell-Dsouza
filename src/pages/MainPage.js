c589d9cca4a55fad9b8af492d9505658237d71fd
Zimam Sayeed <zmmsayeed@gmail.com>
2022-08-24 13:50:46 +0530

    Launching the website

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -13,49 +13,48 @@ import Footer from '../components/Footer';
 import Launch from './Launch';
 
 function MainPage() {
-    // const [mainSectionRef, MainSectionInView] = useInView({ threshold: 0.5 });
-    // const [musicRef, musicInView] = useInView({ threshold: 0.5 });
-    // const [subscribeRef, subscribeInView] = useInView({ threshold: 0.5 });
-    // const [aboutRef, aboutInView] = useInView({ threshold: 0.5 });
-    // const [watchRef, watchInView] = useInView({ threshold: 0.5 });
-    // const [contactRef, contactInView] = useInView({ threshold: 0.5 });
-    // const [galleryRef, galleryInView] = useInView({ threshold: 0.5 });
-    // const [pressRef, pressInView] = useInView({ threshold: 0.5 });
+    const [mainSectionRef, MainSectionInView] = useInView({ threshold: 0.5 });
+    const [musicRef, musicInView] = useInView({ threshold: 0.5 });
+    const [subscribeRef, subscribeInView] = useInView({ threshold: 0.5 });
+    const [aboutRef, aboutInView] = useInView({ threshold: 0.5 });
+    const [watchRef, watchInView] = useInView({ threshold: 0.5 });
+    const [contactRef, contactInView] = useInView({ threshold: 0.5 });
+    const [galleryRef, galleryInView] = useInView({ threshold: 0.5 });
+    const [pressRef, pressInView] = useInView({ threshold: 0.5 });
 
-    // let activeSection = "";
+    let activeSection = "";
 
-    // if (MainSectionInView) 
-    //     activeSection = "mainSection";
-    // else if (musicInView) 
-    //     activeSection = "music";
-    // else if (subscribeInView) 
-    //     activeSection = "subscribe";
-    // else if (aboutInView) 
-    //     activeSection = "about";
-    // else if (contactInView) 
-    //     activeSection = "contact";
-    // else if (watchInView)
-    //     activeSection = "watch";
-    // else if (galleryInView)
-    //     activeSection = "gallery";
-    // else if (pressInView)
-    //     activeSection = "press";
+    if (MainSectionInView) 
+        activeSection = "mainSection";
+    else if (musicInView) 
+        activeSection = "music";
+    else if (subscribeInView) 
+        activeSection = "subscribe";
+    else if (aboutInView) 
+        activeSection = "about";
+    else if (contactInView) 
+        activeSection = "contact";
+    else if (watchInView)
+        activeSection = "watch";
+    else if (galleryInView)
+        activeSection = "gallery";
+    else if (pressInView)
+        activeSection = "press";
 
-    // return (
-    //     <>
-    //         <Navbar activeSection={activeSection} />
-    //         <MainSection refSection={mainSectionRef} />
-    //         <Music refSection={musicRef} />
-    //         <Subscribe refSection={subscribeRef} />
-    //         <About refSection={aboutRef} />
-    //         <Watch refSection={watchRef} />
-    //         <Gallery refSection={galleryRef} />
-    //         <Press refSection={pressRef} />
-    //         <Contact refSection={contactRef} />
-    //         <Footer />
-    //     </>
-    // );
-    return <Launch />
+    return (
+        <>
+            <Navbar activeSection={activeSection} />
+            <MainSection refSection={mainSectionRef} />
+            <Music refSection={musicRef} />
+            <Subscribe refSection={subscribeRef} />
+            <About refSection={aboutRef} />
+            <Watch refSection={watchRef} />
+            <Gallery refSection={galleryRef} />
+            <Press refSection={pressRef} />
+            <Contact refSection={contactRef} />
+            <Footer />
+        </>
+    );
 }
 
 export default MainPage;
